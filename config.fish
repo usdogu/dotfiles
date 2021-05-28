4a96f582e8371de5ee9951eec19121441ee5c75d
usdogu <73913444+usdogu@users.noreply.github.com>
2021-05-28 13:28:35 +0300

    fish config

---
+++ config.fish
@@ -0,0 +1,16 @@
+set PATH /home/dogu/.cargo/bin /usr/local/bin /usr/bin /bin /usr/X11R6/bin ~/.cargo/bin
+alias cls=clear
+alias s=sudo
+alias cc="cargo check"
+alias cr="cargo run"
+alias v=nvim
+alias ls=lsd
+alias ll="lsd -l"
+alias la="lsd -A"
+alias l="lsd -Al"
+alias saat="date | cut -d' ' -f5"
+alias upt="echo '123456' | sudo -S zypper ref && echo '123456' | sudo -S zypper dup"
+alias cat=bat
+alias cut=choose
+alias rm="rip"
+alias sed=sd
