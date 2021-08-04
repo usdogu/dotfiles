fc5cc2c467e15dd8ace5cb847819a341d0d66e01
USPRO <usdogu@gmail.com>
2021-08-04 14:43:15 +0300

    edit fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-set PATH /home/dogu/.cargo/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 alias cls=clear
 alias s=sudo
 alias cc="cargo check"
@@ -10,6 +9,4 @@ alias la="lsd -A"
 alias l="lsd -Al"
 alias saat="date '+%H:%M'"
 alias cat=bat
-alias cut=choose
 alias rm=rip
-alias sed=sd
