2e80836c0c8b273d63dd8ab1b1b97d8b9631cdfc
USPRO <usdogu@gmail.com>
2022-02-08 15:48:07 +0300

    update installer

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 
 
-sudo pacman -Syyyu && sudo pacman -S git emacs ripgrep fd neovim libx11 libxinerama libxft libxext libxrender ttf-hack ttf-joypixels llvm lsd clang fish sxhkd zoxide zellij doas bpytop nyxt
+sudo pacman -Syyyu && sudo pacman -S git emacs ripgrep fd neovim libx11 libxinerama libxft libxext libxrender ttf-hack ttf-joypixels llvm lsd clang fish sxhkd zoxide zellij doas
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
 
 # create necessary dirs
 mkdir -p ~/Derleme && cd ~/Derleme
 mkdir -p ~/bin
 
 #install required things for dwm
-git clone https://github.com/siduck76/st.git
+echo "Installing DWM"
+git clone https://github.com/siduck/st.git
 cd st
 sudo make install
 cd .. && rm -rf st
@@ -21,16 +23,18 @@ cd dmenu-distrotube
 sudo make install
 cd .. && rm -rf dmenu-distrotube
 
-git clone https://github.com/usdogu/rootblocks.git
-cd rootblocks
-cargo build --release
-cp target/release/rootblocks ~/bin/rootblocks
-cd .. && rm -rf rootblocks
+git clone https://github.com/usdogu/nimblocks.git
+cd nimblocks
+nimble build -d:release -d:danger
+cp nimblocks ~/bin/nimblocks
+cd .. && rm -rf nimblocks
 
-echo "Finished installing required packages for dwm."
+git clone https://github.com/usdogu/dwm-dots.git
+cd dwm-dots
+sudo make install
+cd .. && rm -rf dwm-dots
 
 # clone dots
-
 git clone --recurse-submodules https://github.com/usdogu/dotfiles.git
 cd dotfiles
 
@@ -46,8 +50,8 @@ echo "Cloned Font."
 echo "Installing Doom Emacs..."
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
-echo "Doom Emacs installed."
 
+echo "Copying Config Files.."
 cp -r .doom.d ~/
 cp .xinitrc ~
 cp .Xresources ~
@@ -56,9 +60,6 @@ cd .config
 cp starship.toml ~/.config/
 cp -r sxhkd ~/.config/sxhkd
 cp fish/config.fish ~/.config/fish/config.fish
-cp -r nyxt ~/.config/nyxt
-cd dwm-dots
-sudo make install
 
 cd ~
 ~/.emacs.d/bin/doom sync
